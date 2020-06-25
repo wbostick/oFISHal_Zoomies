61035011e238336e26f096c1989754328c0714c3
z5052 <5052ga@gmail.com>
2020-06-25 17:28:51 -0400

    Added rough movement script and some base stuff

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    #region Public Vars
+    public Vector2 tankPosition;
+    public Vector2 targetPoint;
+    #endregion
+
+    #region Private Vars
+    private readonly float timeScaleBase = 0.01f; // Base for the lerp timescale
+    private float timeScale; 
+    private bool isMoving = false;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Makes player move to a point as long as button is held down
+        if(Input.GetButton("Fire1"))  
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+        if(Input.GetButtonDown("Fire1") || Input.GetButtonUp("Fire1"))
+        {
+            timeScale = timeScaleBase;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            MoveToPoint(targetPoint);
+        }
+        else
+        {
+            ReturnToTank();
+        }
+    }
+
+    private void ReturnToTank()
+    {
+        transform.position = Vector2.Lerp(transform.position, tankPosition, timeScale);
+        timeScale += Time.deltaTime / 2;
+    }
+
+    private void MoveToPoint(Vector2 targetPosition)
+    {
+        transform.position = Vector2.Lerp(transform.position, targetPosition, timeScale);
+        timeScale += Time.deltaTime / 6;
+    }
+
+}
---
+++ Assets/Scripts/ZoneCreator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneCreator : MonoBehaviour
+{   // Will create zones by seraching for game objects with tags, the tags determine what zone the object goes into. The game object gives its vector2 transform and stores it in its respective zone
+    #region Public Vars
+    public List<List<Vector2>> zones = new List<List<Vector2>>();
+    public List<Vector2> zoneMovePoints = new List<Vector2>();
+    #endregion
+
+    #region Private Vars
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
