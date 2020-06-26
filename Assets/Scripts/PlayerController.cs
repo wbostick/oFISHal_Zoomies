20f67477036501d8aae5d994545cd8ac1971d9b6
z5052 <5052ga@gmail.com>
2020-06-26 13:12:16 -0400

    Finished the Zone creator and implemented it with the player controller
    Deleted shrek

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,25 @@ public class PlayerController : MonoBehaviour
     #region Public Vars
     public Vector2 tankPosition;
     public Vector2 targetPoint;
+    public int returnSpeedDivider = 2; // Divider to determine speed of fish returning to safe zone
+    public int moveSpeedDivider = 6; // Divider to determine speed of fish moving to its point
+    public static int zoneOneNumerator = 0; // Numerator for numerating across the points in zone 1
+    public static int zoneTwoNumerator = 0; // Numerator for numerating across the points in zone 2
+    public static int zoneThreeNumerator = 0; // Numerator for numerating across the points in zone 3
     #endregion
 
     #region Private Vars
     private readonly float timeScaleBase = 0.01f; // Base for the lerp timescale
-    private float timeScale; 
+    private float timeScale; // For the vector2.Lerp
     private bool isMoving = false;
+    private int zoneNum = 0;
+    private List<List<Vector2>> zoneList = new List<List<Vector2>>(); // List of all the zones, used for getting the target points in a zone
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-
+        zoneList = ZoneCreator.GetZoneList(); // Gets the list of zones from ZoneCreator
     }
 
     // Update is called once per frame
@@ -33,14 +40,31 @@ public class PlayerController : MonoBehaviour
         {
             isMoving = false;
         }
+
+        // Resets the timescale for Vector2.Lerp
         if(Input.GetButtonDown("Fire1") || Input.GetButtonUp("Fire1"))
         {
             timeScale = timeScaleBase;
         }
+
+        // Switches the zone the fish travels too, cannot happen while the move button is held and until it's close enough to the tank position. Pressing the button will switch zones
+        if (!isMoving && (transform.position.magnitude - tankPosition.magnitude) <= Mathf.Epsilon && Input.GetButtonDown("Fire2"))
+        {
+            zoneNum = (zoneNum + 1) % 3;
+        }
+    }
+
+
+    // For the zone system the object itself will have an onTriggerEnter with the fish and then broadcast that it is "destroyed" and moves its respective zone numerator up by 1
+
+    private void LateUpdate()
+    {
+        
     }
 
     private void FixedUpdate()
     {
+        // Calls the functions to move the fish
         if (isMoving)
         {
             MoveToPoint(targetPoint);
@@ -51,16 +75,19 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+
+    // Lerps the fish to tankPosition
     private void ReturnToTank()
     {
         transform.position = Vector2.Lerp(transform.position, tankPosition, timeScale);
-        timeScale += Time.deltaTime / 2;
+        timeScale += Time.deltaTime / returnSpeedDivider;
     }
 
+    // Lerps the fish to targetPosition
     private void MoveToPoint(Vector2 targetPosition)
     {
         transform.position = Vector2.Lerp(transform.position, targetPosition, timeScale);
-        timeScale += Time.deltaTime / 6;
+        timeScale += Time.deltaTime / moveSpeedDivider;
     }
 
 }
--- Assets/Scripts/ZoneCreator.cs
+++ Assets/Scripts/ZoneCreator.cs
@@ -3,26 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ZoneCreator : MonoBehaviour
-{   // Will create zones by seraching for game objects with tags, the tags determine what zone the object goes into. The game object gives its vector2 transform and stores it in its respective zone
+{   // Will create zones by seraching for game objects with tags, the tags determine what zone the object goes into. The game object gives its vector2 transform.position and stores it in its respective zone
     #region Public Vars
-    public List<List<Vector2>> zones = new List<List<Vector2>>();
-    public List<Vector2> zoneMovePoints = new List<Vector2>();
+
     #endregion
 
     #region Private Vars
-
+    private static List<List<Vector2>> zones = new List<List<Vector2>>(); // A list of all the zones 
+    private List<Vector2> zoneOneMovePoints = new List<Vector2>(); // List of Vector2 transform.position for objects in zone 1
+    private List<Vector2> zoneTwoMovePoints = new List<Vector2>(); // List of Vector2 transform.position for objects in zone 2
+    private List<Vector2> zoneThreeMovePoints = new List<Vector2>(); // List of Vector2 transform.position for objects in zone 3
+    private GameObject[] objectsInZone; // Used to find objects in a zone and write their transform.position into a list
     #endregion
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-  
+        // fills in the list zoneOneMovePoints with the transform.position of all objects with the tag "Zone 1"
+        objectsInZone = GameObject.FindGameObjectsWithTag("Zone 1");
+        for(int i = 0; i < objectsInZone.Length; i++)
+        {
+            zoneOneMovePoints.Add(objectsInZone[i].transform.position);
+        }
+        zones.Add(zoneOneMovePoints);
+
+        // fills in the list zoneTwoMovePoints with the transform.position of all objects with the tag "Zone 2"
+        objectsInZone = GameObject.FindGameObjectsWithTag("Zone 2");
+        for (int i = 0; i < objectsInZone.Length; i++)
+        {
+            zoneTwoMovePoints.Add(objectsInZone[i].transform.position);
+        }
+        zones.Add(zoneTwoMovePoints);
+
+        // fills in the list zoneThreeMovePoints with the transform.position of all objects with the tag "Zone 3"
+        objectsInZone = GameObject.FindGameObjectsWithTag("Zone 3");
+        for (int i = 0; i < objectsInZone.Length; i++)
+        {
+            zoneThreeMovePoints.Add(objectsInZone[i].transform.position);
+        }
+        zones.Add(zoneThreeMovePoints);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    // Function for the PlayerController script to access the zone list, returns the nested list zones
+    public static List<List<Vector2>> GetZoneList()
+    {
+        return zones;
     }
 
 }
