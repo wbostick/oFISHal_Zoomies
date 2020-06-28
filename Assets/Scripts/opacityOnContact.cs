8f981c3f5183d574862cf0780e108a86e7c30cf8
z5052 <5052ga@gmail.com>
2020-06-28 16:14:31 -0400

    Merging master to Daniel Branch

---
+++ Assets/Scripts/opacityOnContact.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class opacityOnContact : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    public float opacity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            sprite.color = new Color(1f, 1f, 1f, opacity);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            sprite.color = new Color(1f, 1f, 1f, 1f);
+        }
+    }
+}
---
+++ Assets/Scripts/ownerAnimation.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ownerAnimation : MonoBehaviour
+{
+    public float faceChangeTime;
+    public bool turnActivated = false;
+    public bool ownerIsLooking = false;
+    public Animator animator;
+
+
+    public float[] turnTimes;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(faceRandomizer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void changeFace()
+    {
+        animator.SetInteger("whichOwner", Random.Range(1, 6));
+    }
+
+    public void activateTurn()
+    {
+        turnActivated = true;
+        animator.SetInteger("whichOwner", 0);
+        StartCoroutine(turnPhaseChange(1));
+    }
+
+    IEnumerator faceRandomizer()
+    {
+        yield return new WaitForSeconds(faceChangeTime);
+        if (turnActivated == false)
+        {
+            changeFace();
+            StartCoroutine(faceRandomizer());
+        }
+        
+    }
+    IEnumerator turnPhaseChange(int phase)
+    {
+        yield return new WaitForSeconds(turnTimes[phase - 1]);
+        if (phase == turnTimes.Length)
+        {
+            turnActivated = false;
+            ownerIsLooking = false;
+            changeFace();
+            StartCoroutine(faceRandomizer());
+        }
+        else
+        {
+            if (phase == 2)
+            {
+                ownerIsLooking = true;
+            }
+            animator.SetInteger("turnPhase", phase + 1);
+            StartCoroutine(turnPhaseChange(phase + 1));
+        }
+
+    }
+}
