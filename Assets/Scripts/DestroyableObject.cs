28e8ff76b9a9ba5e683b652d5af504674a21cad2
z5052 <5052ga@gmail.com>
2020-06-28 21:42:14 -0400

    Revert "Merge branch 'master' into Daniel-Branch"
    This reverts commit 52ca7f4bd1b992388177e143d7572ba81771c0f2, reversing
    changes made to b0a35e138e90934ca0ecd25571b8467eee16444b.

--- Assets/Scripts/DestroyableObject.cs
+++ Assets/Scripts/DestroyableObject.cs
@@ -5,46 +5,63 @@ using UnityEngine;
 public class DestroyableObject : MonoBehaviour
 {
     #region Private Vars
-
+    
     #endregion
 
     #region Public Vars
-    public float destroyVelocity = 100.0f;
+    public bool isDestroyed = false;
     #endregion
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player" && PlayerController.isMoving)
         {
+            isDestroyed = true;
             EnumerateZonePoint();
-
-            collision.gameObject?.GetComponent<PlayerController>().KickEvent.Invoke();
-            StartCoroutine(KickOfScreen());
+            PlayerController.TargetChange();
         }
     }
 
     private void EnumerateZonePoint()
     {
-        if (PlayerController.zoneNumPublic == 0 && PlayerController.zoneOneNumerator < ZoneCreator.zoneOneMaxPoints - 1)
+        if (PlayerController.zoneNumPublic == 0)
         {
-            PlayerController.zoneOneNumerator++;
+            if (PlayerController.zoneOneNumerator < ZoneCreator.zoneOneMaxPoints - 1)
+            {
+                PlayerController.zoneOneNumerator++;
+            }
+            else if (PlayerController.zoneOneNumerator < ZoneCreator.zoneOneMaxPoints && isDestroyed)
+            {
+                PlayerController.CallNextZone();
+                PlayerController.isReturning = true;
+                PlayerController.isMoving = false;
+            }
         }
-        else if (PlayerController.zoneNumPublic == 1 && PlayerController.zoneTwoNumerator < ZoneCreator.zoneTwoMaxPoints - 1)
+        else if (PlayerController.zoneNumPublic == 1)
         {
-            PlayerController.zoneTwoNumerator++;
+            if (PlayerController.zoneTwoNumerator < ZoneCreator.zoneTwoMaxPoints - 1)
+            {
+                PlayerController.zoneTwoNumerator++;
+            }
+            else if (PlayerController.zoneTwoNumerator < ZoneCreator.zoneTwoMaxPoints && isDestroyed)
+            {
+                PlayerController.CallNextZone();
+                PlayerController.isReturning = true;
+                PlayerController.isMoving = false;
+            }
         }
-        else if (PlayerController.zoneNumPublic == 2 && PlayerController.zoneThreeNumerator < ZoneCreator.zoneThreeMaxPoints - 1)
+        else if (PlayerController.zoneNumPublic == 2)
         {
-            PlayerController.zoneThreeNumerator++;
+            if (PlayerController.zoneThreeNumerator < ZoneCreator.zoneThreeMaxPoints - 1)
+            {
+                PlayerController.zoneThreeNumerator++;
+            }
+            else if (PlayerController.zoneThreeNumerator < ZoneCreator.zoneThreeMaxPoints && isDestroyed)
+            {
+                PlayerController.CallNextZone();
+                PlayerController.isReturning = true;
+                PlayerController.isMoving = false;
+            }
         }
-    }
-
-    public IEnumerator KickOfScreen()
-    {
-        yield return new WaitForSeconds(.2f);
-        Vector3 dir = transform.position;
-        dir.z = 0;
-        dir.Normalize();
-        GetComponent<Rigidbody2D>().velocity = (destroyVelocity * dir);
     }
 }
