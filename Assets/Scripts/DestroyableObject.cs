b0a35e138e90934ca0ecd25571b8467eee16444b
z5052 <5052ga@gmail.com>
2020-06-28 21:24:55 -0400

    Finished Player Movement

--- Assets/Scripts/DestroyableObject.cs
+++ Assets/Scripts/DestroyableObject.cs
@@ -5,34 +5,63 @@ using UnityEngine;
 public class DestroyableObject : MonoBehaviour
 {
     #region Private Vars
-
+    
     #endregion
 
     #region Public Vars
-
+    public bool isDestroyed = false;
     #endregion
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player" && PlayerController.isMoving)
         {
+            isDestroyed = true;
             EnumerateZonePoint();
+            PlayerController.TargetChange();
         }
     }
 
     private void EnumerateZonePoint()
     {
-        if (PlayerController.zoneNumPublic == 0 && PlayerController.zoneOneNumerator < ZoneCreator.zoneOneMaxPoints - 1)
+        if (PlayerController.zoneNumPublic == 0)
         {
-            PlayerController.zoneOneNumerator++;
+            if (PlayerController.zoneOneNumerator < ZoneCreator.zoneOneMaxPoints - 1)
+            {
+                PlayerController.zoneOneNumerator++;
+            }
+            else if (PlayerController.zoneOneNumerator < ZoneCreator.zoneOneMaxPoints && isDestroyed)
+            {
+                PlayerController.CallNextZone();
+                PlayerController.isReturning = true;
+                PlayerController.isMoving = false;
+            }
         }
-        else if (PlayerController.zoneNumPublic == 1 && PlayerController.zoneTwoNumerator < ZoneCreator.zoneTwoMaxPoints - 1)
+        else if (PlayerController.zoneNumPublic == 1)
         {
-            PlayerController.zoneTwoNumerator++;
+            if (PlayerController.zoneTwoNumerator < ZoneCreator.zoneTwoMaxPoints - 1)
+            {
+                PlayerController.zoneTwoNumerator++;
+            }
+            else if (PlayerController.zoneTwoNumerator < ZoneCreator.zoneTwoMaxPoints && isDestroyed)
+            {
+                PlayerController.CallNextZone();
+                PlayerController.isReturning = true;
+                PlayerController.isMoving = false;
+            }
         }
-        else if (PlayerController.zoneNumPublic == 2 && PlayerController.zoneThreeNumerator < ZoneCreator.zoneThreeMaxPoints - 1)
+        else if (PlayerController.zoneNumPublic == 2)
         {
-            PlayerController.zoneThreeNumerator++;
+            if (PlayerController.zoneThreeNumerator < ZoneCreator.zoneThreeMaxPoints - 1)
+            {
+                PlayerController.zoneThreeNumerator++;
+            }
+            else if (PlayerController.zoneThreeNumerator < ZoneCreator.zoneThreeMaxPoints && isDestroyed)
+            {
+                PlayerController.CallNextZone();
+                PlayerController.isReturning = true;
+                PlayerController.isMoving = false;
+            }
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,17 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     #region Public Vars
     public Vector2 tankPosition;
+    public float tankSize = 2.0f;
     public int returnSpeedDivider = 2; // Divider to determine speed of fish returning to safe zone
     public int moveSpeedDivider = 6; // Divider to determine speed of fish moving to its point
     public static int zoneOneNumerator = 0; // Numerator for numerating across the points in zone 1
     public static int zoneTwoNumerator = 0; // Numerator for numerating across the points in zone 2
     public static int zoneThreeNumerator = 0; // Numerator for numerating across the points in zone 3
     public static int zoneNumPublic;
-    public static bool targetChanged = false;
 
     public static bool isJumping = false;
     public static bool isKicking = false;
     public static bool isMoving = false;
+    public static bool isReturning = false;
     #endregion
 
     #region Private Vars
@@ -25,47 +26,36 @@ public class PlayerController : MonoBehaviour
     private float timeScale; // For the vector2.Lerp
     private int zoneNum = 0;
     private List<List<Vector2>> zoneList = new List<List<Vector2>>(); // List of all the zones, used for getting the target points in a zone
+    private static bool targetChangedPublic = false;
+    private bool targetChanged;
+    private static bool zoneChange = false;
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
+        targetChanged = targetChangedPublic;
         zoneList = ZoneCreator.GetZoneList(); // Gets the list of zones from ZoneCreator
-        if (zoneNum == 0)
-        {
-            targetPoint = zoneList[zoneNum][zoneOneNumerator];
-        }
-        else if (zoneNum == 1)
-        {
-            targetPoint = zoneList[zoneNum][zoneTwoNumerator];
-        }
-        else
-        {
-            targetPoint = zoneList[zoneNum][zoneThreeNumerator];
-        }
+        targetPoint = zoneList[zoneNum][zoneOneNumerator];
     }
 
     // Update is called once per frame
     void Update()
     {
         // Makes player move to a point as long as button is held down
-        if (Input.GetButton("Fire1"))
+        if (Input.GetButton("Fire1") && !isReturning)
         {
             isMoving = true;
         }
         else
         {
+            isReturning = true;
             isMoving = false;
         }
 
-        // Resets the timescale for Vector2.Lerp
-        if (Input.GetButtonDown("Fire1") || Input.GetButtonUp("Fire1"))
-        {
-            timeScale = timeScaleBase;
-        }
-
         /* // 2020 June 28 4:34 PM, Removed zone switching, player automatically moves on to the next zone after destroying all objects in the zone.
-        // Switches the zone the fish travels too, cannot happen while the move button is held and until it's close enough to the tank position. Pressing the button will switch zones
+        // Switches the zone the fish travels too, cannot happen while the move button is held and until it's close enough to the tank position. 
+        // Pressing the button will switch zones
         if (!isMoving && (transform.position.magnitude - tankPosition.magnitude) <= 0.1f && Input.GetButtonDown("Fire2"))
         {
             zoneNum = (zoneNum + 1) % 3;
@@ -89,20 +79,35 @@ public class PlayerController : MonoBehaviour
 
     private void LateUpdate()
     {
-        if (zoneNum == 0)
+        targetChanged = targetChangedPublic;
+        if (targetChanged || ((transform.position.magnitude - tankPosition.magnitude) <= tankSize))
         {
-            targetPoint = zoneList[zoneNum][zoneOneNumerator];
+            if (zoneNum == 0)
+            {
+                targetPoint = zoneList[zoneNum][zoneOneNumerator];
+            }
+            else if (zoneNum == 1)
+            {
+                targetPoint = zoneList[zoneNum][zoneTwoNumerator];
+            }
+            else if (zoneNum == 2)
+            {
+                targetPoint = zoneList[zoneNum][zoneThreeNumerator];
+            }
+            timeScale = timeScaleBase;
+            targetChangedPublic = false;
         }
-        else if (zoneNum == 1)
+
+        if ((transform.position.magnitude - tankPosition.magnitude) < tankSize)
         {
-            targetPoint = zoneList[zoneNum][zoneTwoNumerator];
+            isReturning = false;
         }
-        else
+
+        if (zoneChange)
         {
-            targetPoint = zoneList[zoneNum][zoneThreeNumerator];
+            NextZone();
+            zoneChange = false;
         }
-        targetChanged = false;
-        //timeScale = timeScaleBase;
     }
 
     private void FixedUpdate()
@@ -134,5 +139,21 @@ public class PlayerController : MonoBehaviour
     {
         transform.position = Vector2.Lerp(transform.position, targetPosition, timeScale);
         timeScale += Time.deltaTime / moveSpeedDivider;
+    }
+
+    public static void TargetChange()
+    {
+        targetChangedPublic = !targetChangedPublic;
+    }
+
+    private void NextZone()
+    {
+        zoneNum++;
+        zoneNumPublic = zoneNum;
+    }
+
+    public static void CallNextZone()
+    {
+        zoneChange = true;
     }
 }
