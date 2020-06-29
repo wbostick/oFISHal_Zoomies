90ac14f510211a245ceccc87c25418a68e586f4d
z5052 <5052ga@gmail.com>
2020-06-28 21:48:04 -0400

    Revert "Finished Player Movement"
    This reverts commit b0a35e138e90934ca0ecd25571b8467eee16444b.

--- Assets/Scripts/DestroyableObject.cs
+++ Assets/Scripts/DestroyableObject.cs
@@ -5,63 +5,34 @@ using UnityEngine;
 public class DestroyableObject : MonoBehaviour
 {
     #region Private Vars
-    
+
     #endregion
 
     #region Public Vars
-    public bool isDestroyed = false;
+
     #endregion
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player" && PlayerController.isMoving)
         {
-            isDestroyed = true;
             EnumerateZonePoint();
-            PlayerController.TargetChange();
         }
     }
 
     private void EnumerateZonePoint()
     {
-        if (PlayerController.zoneNumPublic == 0)
+        if (PlayerController.zoneNumPublic == 0 && PlayerController.zoneOneNumerator < ZoneCreator.zoneOneMaxPoints - 1)
         {
-            if (PlayerController.zoneOneNumerator < ZoneCreator.zoneOneMaxPoints - 1)
-            {
-                PlayerController.zoneOneNumerator++;
-            }
-            else if (PlayerController.zoneOneNumerator < ZoneCreator.zoneOneMaxPoints && isDestroyed)
-            {
-                PlayerController.CallNextZone();
-                PlayerController.isReturning = true;
-                PlayerController.isMoving = false;
-            }
+            PlayerController.zoneOneNumerator++;
         }
-        else if (PlayerController.zoneNumPublic == 1)
+        else if (PlayerController.zoneNumPublic == 1 && PlayerController.zoneTwoNumerator < ZoneCreator.zoneTwoMaxPoints - 1)
         {
-            if (PlayerController.zoneTwoNumerator < ZoneCreator.zoneTwoMaxPoints - 1)
-            {
-                PlayerController.zoneTwoNumerator++;
-            }
-            else if (PlayerController.zoneTwoNumerator < ZoneCreator.zoneTwoMaxPoints && isDestroyed)
-            {
-                PlayerController.CallNextZone();
-                PlayerController.isReturning = true;
-                PlayerController.isMoving = false;
-            }
+            PlayerController.zoneTwoNumerator++;
         }
-        else if (PlayerController.zoneNumPublic == 2)
+        else if (PlayerController.zoneNumPublic == 2 && PlayerController.zoneThreeNumerator < ZoneCreator.zoneThreeMaxPoints - 1)
         {
-            if (PlayerController.zoneThreeNumerator < ZoneCreator.zoneThreeMaxPoints - 1)
-            {
-                PlayerController.zoneThreeNumerator++;
-            }
-            else if (PlayerController.zoneThreeNumerator < ZoneCreator.zoneThreeMaxPoints && isDestroyed)
-            {
-                PlayerController.CallNextZone();
-                PlayerController.isReturning = true;
-                PlayerController.isMoving = false;
-            }
+            PlayerController.zoneThreeNumerator++;
         }
     }
 }
