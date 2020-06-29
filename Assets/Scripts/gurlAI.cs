5510660944a0b224e4a908b1c026b8da67e88e12
navsab <navsab@umich.edu>
2020-06-28 21:54:41 -0400

    idk

---
+++ Assets/Scripts/gurlAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gurlAI : MonoBehaviour
+{
+    ownerAnimation animation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animation = GetComponent<ownerAnimation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
